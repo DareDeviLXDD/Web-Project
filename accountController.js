42dd8050819532c4bacad9fed80090c6d6a6a0c9
Mostafa Mounib <155090521+mostafamounib@users.noreply.github.com>
2024-06-24 02:58:36 +0300

    Add files via upload

--- accountController.js
+++ accountController.js
@@ -1,89 +1,25 @@
 const userModel = require('../models/userModel');
-const moment = require('moment');
+const domainName= '3vk5q7h7-8080.uks1.devtunnels.ms';
 const express = require('express');
-const domainName = '3vk5q7h7-8080.uks1.devtunnels.ms';
 const router = express.Router();
-const orderModel=require('../models/orderModel');
+const moment = require('moment'); 
 
 
-// Function to render account page with user data
-const getAccount = async (req, res) => {
-  try {
-    const users = await User.find(); // Assuming this fetches all users
-    res.render('account', { users, moment, domainName });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(`An error occurred while fetching account data: ${err.message}`);
-  }
-};
-
-
-// Function to update username
-const updateUsername = async (req, res) => {
-  const { newUsername } = req.body;
-  const userId = req.params.userId;
-
-  try {
-    const user = await User.findByIdAndUpdate(
-      userId,
-      { username: newUsername },
-      { new: true }
-    );
-
-    if (!user) {
-      return res.status(404).send('User not found');
+// check if user is logged in
+exports.ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect('/login');
     }
-
-    res.status(200).send('Username updated successfully');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(`Error updating username: ${err.message}`);
-  }
-};
-
-// Function to update password
-const updatePassword = async (req, res) => {
-  const { newPassword } = req.body;
-  const userId = req.params.userId;
-
-  try {
-    const user = await User.findByIdAndUpdate(
-      userId,
-      { password: newPassword },
-      { new: true }
-    );
-
-    if (!user) {
-      return res.status(404).send('User not found');
+  };
+  //get account details
+  exports.getAccountDetails = async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id);
+      res.render('account', { user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
     }
-
-    res.status(200).send('Password updated successfully');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(`Error updating password: ${err.message}`);
-  }
-};
-const getOrderHistory = async (req, res) => {
-  try {
-    const userId = req.params.userId; // Assuming you're passing userId in the route params
-    const user = await User.findById(userId).populate('orders'); // Assuming orders are referenced in the user model
-
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
-
-    res.render('account', { users: [user], moment, domainName }); // Passing user object to the account page
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(`An error occurred while fetching order history: ${err.message}`);
-  }
-};
-
-
-
-module.exports = {
-  getAccount,
-  updateUsername,
-  updatePassword,
-  getOrderHistory,
-};
+  };
