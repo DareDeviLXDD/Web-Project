84f15eba3b84fd60672abacec540861487d8a189
Mostafa Mounib <155090521+mostafamounib@users.noreply.github.com>
2024-06-24 00:08:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function showSuccessMessage(sectionId, message) {
-    var successMessage = document.getElementById(sectionId + '-success-message');
-    successMessage.textContent = message;
-    successMessage.style.display = 'block';
-    setTimeout(function() {
-        successMessage.style.display = 'none';
-    }, 5000); // Hide the message after 5 seconds
-    return false; // Prevent form submission
-}
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    // Perform your login logic here (e.g., sending the data to the server)
+    alert(`Email: ${email}\nPassword: ${password}`);
+});
+
+document.getElementById('sign-up-button').addEventListener('click', function() {
+    // Redirect to the sign-up page
+    window.location.href = 'signup.html'; // Change this to the URL of your sign-up page
+});
